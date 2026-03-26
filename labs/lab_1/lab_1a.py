939b566e01c2c6d91617001f79632e01a21a8f84
alexl1239 <aluo27@choate.edu>
2026-03-26 13:21:16 -0400

    added intro to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Alex" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone, I'm Alex Luo and I'm excited for the opportunity to take part on this program with yall. I'm really passionate about robotics, particularly competition robotics. I'm on my school's FRC team (go 7407!)")
 
 if __name__ == "__main__":
     main()
